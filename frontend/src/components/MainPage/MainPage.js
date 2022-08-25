97e72a021d40ea233e0bfcdf087146b5869190ff
shreyapp <shreyapatil3004@gmail.com>
2022-08-25 11:39:30 +0530

    sp1

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -7,6 +7,7 @@ export default function MainPage(props){
 
     return(
         <>
+        
         </>
     )
 }
--- frontend/urls.py
+++ frontend/urls.py
@@ -3,9 +3,7 @@ from .views import index
 
 urlpatterns = [
     path('',index),
-<<<<<<< HEAD
     path('homepage',index),
-=======
     path('login/',index),
->>>>>>> d68916b891fa515aa6dc3c6e18b41615e4ecd553
+
 ]
