99333d670d319ee2356c0fc980f2b8df634051d1
shreyapp <shreyapatil3004@gmail.com>
2022-08-25 12:00:40 +0530

    shreya2

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from "react";
 import Table from "../Table_Component/Table";
+import { useLocation,useNavigate } from "react-router-dom";
 
 
 export default function MainPage(props){
@@ -8,6 +9,8 @@ export default function MainPage(props){
     const [sorted,setSorted] = useState()
     const [loading,setloading] = useState(false)
 
+    const location = useLocation()
+    const navigate = useNavigation()
 
     /* Data Caller Runs on Page load */
     useEffect(()=>{
@@ -18,9 +21,17 @@ export default function MainPage(props){
             setData(data)
             console.log(data)
         }
-        runer()
+        if(location.state.user=== null){
+            navigate('homepage/buffer')
+        }
+        else
+            runer()
     },[])
 
+    const TableComponent=(e)=>{
+        //console.log(sorted[e])
+        navigate('/homepage/page',{ state : {'sorted' : sorted[e],'user' : location.state.user}})
+      }
 
     // Runs on data change 
     useEffect(()=>{
@@ -32,7 +43,7 @@ export default function MainPage(props){
 
     return(
         <>
-        
+            {load ? <Table data={sorted} TableComponent={TableComponent}/> : <Loader />}
         </>
     )
 }
--- frontend/urls.py
+++ frontend/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     path('',index),
     path('homepage',index),
     path('login/',index),
-<<<<<<< HEAD
-
-=======
->>>>>>> 198190e54e6fd70ddd12b8ef8a3fcf4e6159c232
 ]
